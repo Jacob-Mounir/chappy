6a27bc4b8b9f0c949e7b296b468374f11af7344b
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2025-01-16 22:23:07 +0100

    fixed CORS issues

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,7 +22,8 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'https://chappy-frontend.onrender.com',
-    'https://chappyv.onrender.com'
+    'https://chappyv.onrender.com',
+    'https://chappy-frontend.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
